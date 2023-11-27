f453c5eb77a1bab6d374ecefb3561546a8319d6e
Toronto22 <toronto.nigma@gmail.com>
2023-11-27 21:52:36 +0700

    Update Github action java level

--- src/test/java/bankproject/automation/helper/webdriver/localdriver/LocalDriverFirefox.java
+++ src/test/java/bankproject/automation/helper/webdriver/localdriver/LocalDriverFirefox.java
@@ -1,16 +1,16 @@
 package bankproject.automation.helper.webdriver.localdriver;
 
 import bankproject.automation.helper.webdriver.Driver;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 
-import static bankproject.automation.model.DriverPath.FIREFOX_DRIVER_PATH;
 
 public class LocalDriverFirefox implements Driver {
     @Override
     public WebDriver init() {
-        System.setProperty("webdriver.gecko.driver", FIREFOX_DRIVER_PATH);
+        WebDriverManager.firefoxdriver().setup();
         FirefoxOptions firefoxOptions = new FirefoxOptions();
 
         if (config.IsHeadless) {
